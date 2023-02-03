c805f7da917bbff73aac5fb3f23950264a67a5aa
Justin <57725347+juskek@users.noreply.github.com>
2023-02-03 11:25:39 +0000

    update

--- bricks/repository/__brick__/{{data_type.lowerCase().snakeCase()}}_remote_data_source.dart
+++ bricks/repository/__brick__/{{data_type.lowerCase().snakeCase()}}_remote_data_source.dart
@@ -2,6 +2,6 @@ export 'package:injectable/injectable.dart';
 import './i_{{data_type.lowerCase().snakeCase()}}_repository.dart';
 
 @singleton
-class {{data_type.pascalCase()}}LocalDataSource {
+class {{data_type.pascalCase()}}RemoteDataSource {
   
 }
--- bricks/repository/__brick__/{{data_type.lowerCase().snakeCase()}}_repository.dart
+++ bricks/repository/__brick__/{{data_type.lowerCase().snakeCase()}}_repository.dart
@@ -1,11 +1,13 @@
 export 'package:injectable/injectable.dart';
 import './i_{{data_type.lowerCase().snakeCase()}}_repository.dart';
+import './{{data_type.lowerCase().snakeCase()}}_local_data_source.dart';
+import './{{data_type.lowerCase().snakeCase()}}_remote_data_source.dart';
 
 @Singleton(as: I{{data_type.pascalCase()}}Repository)
 class {{data_type.pascalCase()}}Repository implements I{{data_type.pascalCase()}}Repository {
   {{data_type.pascalCase()}}Repository(this._{{data_type.camelCase()}}LocalDataSource,this._{{data_type.camelCase()}}RemoteDataSource);
 
-  final _{{data_type.pascalCase()}}LocalDataSource _{{data_type.camelCase()}}LocalDataSource;
-  final _{{data_type.pascalCase()}}RemoteDataSource _{{data_type.camelCase()}}RemoteDataSource;
+  final {{data_type.pascalCase()}}LocalDataSource _{{data_type.camelCase()}}LocalDataSource;
+  final {{data_type.pascalCase()}}RemoteDataSource _{{data_type.camelCase()}}RemoteDataSource;
 
 }
